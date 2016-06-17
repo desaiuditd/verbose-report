00c4db71328c7d89bb14724b88b61c3e1bf9c182
Udit Desai <desaiuditd@gmail.com>
2016-06-17 15:09:50 -0700

    fixes logout issue

--- client/events/appNav.js
+++ client/events/appNav.js
@@ -1,12 +1,12 @@
 /**
- * Created by udit on 19/05/16.
+ * Created by udit on 17/06/16.
  */
 Template.appNav.events(
 	{
 		'click .js-logout': function() {
 			if ( Meteor.userId() ) {
 				AccountsTemplates.logout();
-				FlowRouter.go('/login');
+				FlowRouter.go('/');
 			}
 		}
 	}
